713929c09410b9873dcdb31b831a5c1fdce769a4
Bá Đại <lebadai23042005@gmail.com>
2024-10-07 15:00:52 +0700

    bai 1 tuan 6

---
+++ bai1.java
@@ -0,0 +1,11 @@
+package tuan6;
+	  public class bai1 {
+	  public static void main(String[] args) {
+	        HinhChuNhat hinhChuNhat = new HinhChuNhat(5, 3);
+	        double dienTich = hinhChuNhat.tinhDienTich();
+	        double chuVi = hinhChuNhat.tinhChuVi();
+	        System.out.println("Diện tích hình chữ nhật: " + dienTich);
+	        System.out.println("Chu vi hình chữ nhật: " + chuVi);
+	    }
+	}
+
